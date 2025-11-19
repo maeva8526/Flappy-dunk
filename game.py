bd816596ae2c837d22ce42df8502a9ee8bbdea25
Maéva GIRARD <maevagi85@gmail.com>
2025-11-19 09:09:44 +0100

    ajout background

--- game.py
+++ game.py
@@ -1,9 +1,14 @@
 import pygame
 
+background_layers = [
+    pygame.image.load("fond-_ecran.png").convert(),
+    pygame.image.load().convert()
+]
+
 class Game:
     def __init__(self):
         self.width = 430 #largeur de la fenêtre
-        self.height = 680 #hauteur de la fenêtre
+        self.height = 670 #hauteur de la fenêtre
         self.window = pygame.display.set_mode((self.width, self.height))
         #crée la fenêtre 
         pygame.display.set_caption("Flappy dunk de Maéva et Raphaël")
@@ -18,7 +23,7 @@ class Game:
             for event in pygame.event.get(): #on parcours tous les événements envoyés par pygame
                 if event.type == pygame.QUIT: #si l'utilisateur ferme la fenêtre
                     self.running = False #arrête la boucle 
-            self.window.fill((134, 127, 107)) # couleur des images de la fenêtre
+            self.window.fill((0, 0, 0)) # couleur des images de la fenêtre
             pygame.display.flip() #met à jour l'affichage
             self.clock.tick(60) #limite la boucle à 60 img/sec
         pygame.quit() #ferme pygame quand la boucle est terminée
